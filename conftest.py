1c88cccd34131f22c1a4d40fa6fbbcfefb5a7f06
Alexander Pacha <alexander.pacha@gmail.com>
2018-10-31 16:54:22 +0100

    Excluding annotator_model.py from list of collected tests

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
 collect_ignore = ["MUSCIMarker/MUSCIMarkerApp.py",
-                  "MUSCIMarker/main.py"
+                  "MUSCIMarker/main.py",
+                  "MUSCIMarker/annotator_model.py"
                   ]
