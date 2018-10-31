35038054d3c931e2b1cafed81037c1333cb2fce3
Alexander Pacha <alexander.pacha@gmail.com>
2018-10-31 17:06:46 +0100

    Excluding all files from collection, that import "from kivy.app import App"

--- conftest.py
+++ conftest.py
@@ -1,4 +1,11 @@
 collect_ignore = ["MUSCIMarker/MUSCIMarkerApp.py",
                   "MUSCIMarker/main.py",
-                  "MUSCIMarker/annotator_model.py"
+                  "MUSCIMarker/annotator_model.py",
+                  "MUSCIMarker/cropobject_view.py",
+                  "MUSCIMarker/objid_selection.py",
+                  "MUSCIMarker/toolkit.py",
+                  "MUSCIMarker/mlclass_selection.py",
+                  "MUSCIMarker/edge_view.py",
+                  "MUSCIMarker/rendering.py",
+                  "MUSCIMarker/editor.py",
                   ]
